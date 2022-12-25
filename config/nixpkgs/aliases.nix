2fd198c4316ed52486541a1cabd03593dc36bd9c
Farseen <oss@itsfarseen.anonaddy.me>
2022-12-25 19:23:44 +0530

    alias hme: edit home.nix

--- config/nixpkgs/aliases.nix
+++ config/nixpkgs/aliases.nix
@@ -24,6 +24,7 @@
     glg = "git log";
     # nix
     hms = "home-manager switch";
+    hme = "vim ~/.config/nixpkgs/home.nix";
     nxsw = "sudo nixos-rebuild switch";
     nxs = "nix search nixpkgs";
     # cd
