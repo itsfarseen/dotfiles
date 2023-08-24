36a18ec303add094f8ef4a65f965bda65ea41746
Farseen <oss@itsfarseen.anonaddy.me>
2023-08-24 14:46:57 +0530

    neovim: Detach doc buffers from the LSP

--- config/nvim/lua/plugins/rust.lua
+++ config/nvim/lua/plugins/rust.lua
@@ -15,7 +15,20 @@ return function(use)
 					inlay_hints = { only_current_line = false },
 				},
 				server = {
-					on_attach = on_attach,
+					on_attach = function(client, bufnr)
+						local bufname = vim.api.nvim_buf_get_name(bufnr)
+						local home = vim.env.HOME
+						local folders = { ".rustup", ".cargo" }
+						for _, folder in ipairs(folders) do
+							if bufname:find(home .. "/" .. folder, 1) == 1 or bufname:find("~/" .. folder, 1) == 1 then
+								vim.schedule(function()
+									vim.lsp.buf_detach_client(bufnr, client.id)
+								end)
+								break
+							end
+						end
+						on_attach(client, bufnr)
+					end,
 					settings = {
 						["rust-analyzer"] = {
 							cargo = {
