92fffbc6f4f3069a4d43ff0ce8730a300adb2d50
Farseen <itsfarseen@gmail.com>
2023-10-08 00:36:42 +0530

    bash: move from nix to plain old config

--- home/install.sh
+++ home/install.sh
@@ -3,6 +3,7 @@
 cd $(dirname $0)
 
 ln -nsf $(realpath ./bashrc) $HOME/.bashrc
+ln -nsf $(realpath ./bash_profile) $HOME/.bash_profile
 ln -nsf $(realpath ./xprofile) $HOME/.xprofile
 mkdir -p $HOME/.cargo
 ln -nsf $(realpath ./cargo-config.toml) $HOME/.cargo/config.toml
