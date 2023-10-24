20a32e9a2e9a4c26403665068584215ddbd346c4
Farseen <itsfarseen@gmail.com>
2023-10-24 15:30:57 +0530

    fish: pacman auto suggestion

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,23 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+function pacman_correct --argument-names cmdline --on-event fish_postexec
+    if test $status -eq 0
+        return
+    end
+
+    if not command -q pacman
+        return
+    end
+
+    if string match -q 'sudo pacman -S *' "$cmdline"
+        echo
+        set search_cmdline $(string replace ' -S ' ' -Ss ' "$cmdline")
+        eval "command $search_cmdline"
+        echo
+        echo "Looks like you were trying to search for a package and it's not found."
+        echo "Please see the list of similar packages above."
+        echo
+    end
+end
