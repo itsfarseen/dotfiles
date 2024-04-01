2fb42e0b0f562929a43f564ccb71a24855083d3d
Farseen <itsfarseen@gmail.com>
2024-04-01 23:54:55 +0530

    sway: autostart: update gamma

--- config/sway/autostart.sh
+++ config/sway/autostart.sh
@@ -19,3 +19,8 @@ xsettingsd &
 foot --server &
 /usr/lib/lxpolkit/lxpolkit & 
 waybar &
+# wlr-gammactl-fzn -c 0.97:1.00:0.95 -g 1.015:1.01:1.0 -b 1.01:1.005:0.99
+# wlr-gammactl-fzn -c 0.98:1.00:0.94 -g 1.015:1.01:1.0 -b 1.02:1.005:0.98
+# wlr-gammactl-fzn -c 1.1:1.1:1.1 -g 1.1:1.1:1.1 -b 0.915:0.915:0.9
+# /usr/local/bin/wlr-gammactl-fzn -c 1.01:1.00:0.95 -g 0.95:0.95:0.95 -b 0.97:0.97:0.95
+/usr/local/bin/wlr-gammactl-fzn -c 1.01:1.04:0.86 -b 0.94:0.95:0.88 -g 0.93:0.88:1.10
