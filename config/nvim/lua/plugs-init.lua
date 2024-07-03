f411d42d9320f545086d24e7614afe9f1e3c56ed
Farseen <oss@itsfarseen.anonaddy.me>
2024-07-03 14:51:21 +0530

    nvim: Remove prettier. Use none-ls (null-ls fork) to provide prettier. This makes null-ls config extensible.

--- config/nvim/lua/plugs-init.lua
+++ config/nvim/lua/plugs-init.lua
@@ -6,40 +6,6 @@ local on_attach = require("config.lsp-keybindings").on_attach;
 require("null-ls").setup({
 	on_attach = on_attach,
 })
-require("prettier").setup({
-	bin = 'prettier', -- or `prettierd`
-	filetypes = {
-		"css",
-		"graphql",
-		"html",
-		"javascript",
-		"javascriptreact",
-		"json",
-		"less",
-		"markdown",
-		"scss",
-		"typescript",
-		"typescriptreact",
-		"yaml",
-	},
-	-- prettier format options (you can use config files too. ex: `.prettierrc`)
-	arrow_parens = "always",
-	bracket_spacing = true,
-	embedded_language_formatting = "auto",
-	end_of_line = "lf",
-	html_whitespace_sensitivity = "css",
-	jsx_bracket_same_line = false,
-	jsx_single_quote = false,
-	print_width = 80,
-	prose_wrap = "preserve",
-	quote_props = "as-needed",
-	semi = true,
-	single_quote = false,
-	tab_width = 2,
-	trailing_comma = "es5",
-	use_tabs = false,
-	vue_indent_script_and_style = false,
-})
 require("rust-tools").setup({
 	tools = {
 		autoSetHints = true,
--- config/nvim/lua/plugs/prettier.lua
+++
@@ -1,4 +0,0 @@
-return {
-	'jose-elias-alvarez/null-ls.nvim',
-	'MunifTanjim/prettier.nvim',
-}
