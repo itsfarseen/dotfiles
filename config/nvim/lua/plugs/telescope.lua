1179a5f44f549fe7c6d1dfa1156c1905288da3c8
Farseen <oss@itsfarseen.anonaddy.me>
2024-07-03 14:53:26 +0530

    nvim: use native fzf plugin for telescope. This speeds up filtering.

--- config/nvim/lua/plugs/telescope.lua
+++ config/nvim/lua/plugs/telescope.lua
@@ -1,8 +1,12 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+		},
 		config = function()
+			require('telescope').load_extension('fzf')
 			vim.cmd([[
 					nnoremap <c-p> <cmd>Telescope find_files<cr>
 					nnoremap <leader>ff <cmd>Telescope find_files<cr>
