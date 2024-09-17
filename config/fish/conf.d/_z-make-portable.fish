48cfabe65d0cbbfc9b12868013977fe1d297546e
Farseen <oss@itsfarseen.anonaddy.me>
2024-09-17 16:55:09 +0530

    fish: use $XDG_DATA_HOME instead of $HOME/.local/share if available

--- config/fish/conf.d/_z-make-portable.fish
+++ config/fish/conf.d/_z-make-portable.fish
@@ -2,5 +2,9 @@
 # set -U stores the actual value of $HOME in the fish_variables file
 # which is tracked in the dotfiles repo.
 # This makes it non-portable.
-set -x Z_DATA $HOME/.local/share/z/data
+if test -z "$XDG_DATA_HOME"
+    set -U Z_DATA "$HOME/.local/share/z/data"
+else
+    set -U Z_DATA "$XDG_DATA_HOME/z/data"
+end
 set -xp Z_EXCLUDE $HOME
