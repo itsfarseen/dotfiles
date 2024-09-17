20f10f182d5d6f5ecfb7bfeedb6bb2d055e4d8de
Farseen <oss@itsfarseen.anonaddy.me>
2024-09-17 17:29:31 +0530

    fish: fix Z_DATA var setting logic

--- config/fish/conf.d/_z-make-portable.fish
+++ config/fish/conf.d/_z-make-portable.fish
@@ -3,8 +3,10 @@
 # which is tracked in the dotfiles repo.
 # This makes it non-portable.
 if test -z "$XDG_DATA_HOME"
-    set -U Z_DATA "$HOME/.local/share/z/data"
+    set -U Z_DATA_DIR "$HOME/.local/share/z"
 else
-    set -U Z_DATA "$XDG_DATA_HOME/z/data"
+    set -U Z_DATA_DIR "$XDG_DATA_HOME/z"
 end
+set -U Z_DATA "$Z_DATA_DIR/data"
+
 set -xp Z_EXCLUDE $HOME
