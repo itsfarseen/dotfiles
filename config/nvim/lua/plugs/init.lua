b3a310f1b979df9ba0a4dc884ec12a021b96169f
Farseen <oss@itsfarseen.anonaddy.me>
2024-10-03 10:59:57 +0530

    nvim: Remove lsp-signature, iron-repl
    lsp-signature: This things slowed down vim big time. After about 15
    minutes of usage, every keystroke would take 2 second to process.
    
    iron-repl: I never used it. Don't even know how it works.

--- config/nvim/lua/plugs/init.lua
+++ config/nvim/lua/plugs/init.lua
@@ -21,12 +21,5 @@ return {
 	-- Flutter
 	"akinsho/flutter-tools.nvim",
 	{ "numToStr/Comment.nvim", config = function() require("Comment").setup() end },
-	"tpope/vim-abolish",        -- Find/Replace variants of a word
-	"axvr/zepl.vim",            -- Iron REPL
-	{
-		"ray-x/lsp_signature.nvim", -- get lsp function hints as you type
-		event = "VeryLazy",
-		opts = {},
-		config = function(_, opts) require 'lsp_signature'.setup(opts) end
-	},
+	"tpope/vim-abolish", -- Find/Replace variants of a word
 };
