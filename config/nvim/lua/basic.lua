ed75051e05aec0217e9a64be3d8d1ce17658def2
Farseen <oss@itsfarseen.anonaddy.me>
2024-10-03 10:54:15 +0530

    nvim: Add alternatives to space (leader) based chords
    My keyboard sucks. Especially the space bar, misses space a lot.

--- config/nvim/lua/basic.lua
+++ config/nvim/lua/basic.lua
@@ -15,13 +15,26 @@ vim.cmd([[
   nnoremap <leader>k  :wincmd k <CR>
   nnoremap <leader>j  :wincmd j <CR>
 
+  nnoremap <C-l>  :wincmd l <CR>
+  nnoremap <C-h>  :wincmd h <CR>
+  nnoremap <C-k>  :wincmd k <CR>
+  nnoremap <C-j>  :wincmd j <CR>
+
   nnoremap <leader>wl  :rightbelow vsplit <CR>
   nnoremap <leader>wh  :leftabove vsplit <CR>
   nnoremap <leader>wk  :leftabove split <CR>
   nnoremap <leader>wj  :rightbelow split <CR>
 
+  nnoremap <C-S-l>  :rightbelow vsplit <CR>
+  nnoremap <C-S-h>  :leftabove vsplit <CR>
+  nnoremap <C-S-k>  :leftabove split <CR>
+  nnoremap <C-S-j>  :rightbelow split <CR>
+
   nnoremap <leader>,  :bprev <CR>
   nnoremap <leader>.  :bnext <CR>
+
+  nnoremap <C-,>  :bprev <CR>
+  nnoremap <C-.>  :bnext <CR>
 
   nnoremap <leader>;  :tabprev <CR>
   nnoremap <leader>'  :tabnext <CR>
@@ -31,6 +44,9 @@ vim.cmd([[
 
   nnoremap <c-s> :w<CR>
   nnoremap <leader>c :close<CR>
+  nnoremap <C-S-d> :close<CR>
+  nnoremap <leader>x <cmd>Bdelete<cr>
+  nnoremap <C-S-x> <cmd>Bdelete<cr>
 
   nnoremap <leader><leader> :noh<CR>:pclose<CR>
 
@@ -38,4 +54,6 @@ vim.cmd([[
 ]])
 
 vim.o.completeopt = "menu,menuone,noselect"
+vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = "yes"
 vim.opt.signcolumn = "yes"
