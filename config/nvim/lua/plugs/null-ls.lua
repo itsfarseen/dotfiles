a90351726b55e9ffe36e858abac710b0cdfe7e20
Farseen <oss@itsfarseen.anonaddy.me>
2025-01-04 20:58:56 +0530

    nvim: enable djlint in null lsp for django templates

--- config/nvim/lua/plugs/null-ls.lua
+++ config/nvim/lua/plugs/null-ls.lua
@@ -4,7 +4,11 @@ return {
         config = function()
             local null_ls = require("null-ls")
             null_ls.setup({
-                sources = null_ls.builtins.formatting.prettierd
+                sources = {
+                    null_ls.builtins.formatting.prettierd,
+                    null_ls.builtins.diagnostics.djlint,
+                    null_ls.builtins.formatting.djlint,
+                }
             })
         end,
         dependencies = { "nvim-lua/plenary.nvim" },
