8edeeef9156faf32cd0fb673f35ba17ba0b1b554
Farseen <oss@itsfarseen.anonaddy.me>
2025-01-04 21:03:02 +0530

    fish: ghcup stuff

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/farseen/.ghcup/bin $PATH # ghcup-env
