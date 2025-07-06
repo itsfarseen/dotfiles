90354ca1fc9504f499c9333b941adeeab9ff5ca7
Farseen <oss@itsfarseen.anonaddy.me>
2025-07-07 00:56:25 +0530

    neovim: replace rust-tools with rustaceanvim
    rust-tools is archived.

--- config/nvim/lua/plugs-init.lua
+++ config/nvim/lua/plugs-init.lua
@@ -1,23 +1,3 @@
 require("neoconf").setup()
 require("fidget").setup(require("fidget-config"))
 require("config.lsp-config")
-
-local on_attach = require("config.lsp-keybindings").on_attach
-require("rust-tools").setup({
-	tools = {
-		autoSetHints = true,
-		inlay_hints = { only_current_line = false },
-	},
-	server = {
-		on_attach = on_attach,
-		settings = {
-			["rust-analyzer"] = {
-				cargo = {
-					-- features = {"test-bpf"},
-					-- features = "all",
-					features = {},
-				},
-			},
-		},
-	},
-})
--- config/nvim/lua/plugs/rust.lua
+++ config/nvim/lua/plugs/rust.lua
@@ -1,8 +1,34 @@
+local on_attach = require("config.lsp-keybindings").on_attach
+vim.g.rustaceanvim = {
+	tools = {
+		autoSetHints = true,
+		inlay_hints = { only_current_line = false },
+	},
+	server = {
+		on_attach = on_attach,
+		default_settings = {
+			["rust-analyzer"] = {
+				cargo = {
+					-- features = {"test-bpf"},
+					-- features = "all",
+					features = {},
+				},
+			},
+		},
+	},
+}
+
 return {
 	{
 		"saecki/crates.nvim",
-		dependencies = { 'nvim-lua/plenary.nvim' },
-		config = function() require("crates").setup() end
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("crates").setup()
+		end,
 	},
-	"simrat39/rust-tools.nvim",
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^6", -- Recommended
+		lazy = false, -- This plugin is already lazy
+	},
 }
