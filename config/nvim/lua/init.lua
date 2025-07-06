638d37d6122014f09949d1a275ad6dcfac538b76
Farseen <oss@itsfarseen.anonaddy.me>
2025-07-07 01:02:49 +0530

    neovim: remove unused code
    Show documentation is now provided by LSP.
    See LSP keybindings.
    
    Hover is also provided by LSP, but on <space>d
    
    The border stuff was unused code. So removing.

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -23,56 +23,6 @@ require("keys")
 require("winseperator")
 require("fold")
 
--- Show documentation (K) {{{
--- vim.api.nvim_set_keymap("n", "K", ":lua ShowDocumentation()<cr>", { noremap = true, silent = true })
--- function ShowDocumentation()
--- local filetype = vim.bo.filetype
--- if vim.tbl_contains({ "vim", "help" }, filetype) then
--- vim.cmd("h " .. vim.fn.expand("<cword>"))
--- elseif vim.tbl_contains({ "man" }, filetype) then
--- vim.cmd("Man " .. vim.fn.expand("<cword>"))
--- elseif vim.fn.expand("%:t") == "Cargo.toml" then
--- require("crates").show_popup()
--- else
--- vim.lsp.buf.hover()
--- end
--- end
-
--- }}}
-
--- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
--- virtual_text = true,
--- underline = true,
--- signs = true,
--- })
--- vim.cmd([[
---   set updatetime=300
--- augroup cursor_hover
--- autocmd!
---   autocmd CursorHold * lua vim.diagnostic.open_float(nil, {border = "single", focusable = false})
---   autocmd CursorHoldI * silent! lua vim.lsp.buf.signature_help()
--- augroup end
--- ]])
-
--- Key binds
-
-local border = {
-	{ "🭽", "FloatBorder" },
-	{ "▔", "FloatBorder" },
-	{ "🭾", "FloatBorder" },
-	{ "▕", "FloatBorder" },
-	{ "🭿", "FloatBorder" },
-	{ "▁", "FloatBorder" },
-	{ "🭼", "FloatBorder" },
-	{ "▏", "FloatBorder" },
-}
-
--- LSP settings (for overriding per client)
-local handlers = {
-	["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
-	["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
-}
-
 -- To highlight codefences returned from denols
 vim.g.markdown_fenced_languages = {
 	"ts=typescript",
