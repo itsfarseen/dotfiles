831dcfd3d9b5aec57e38124ddfa973adcd295ae7
Farseen <farseen@Farseens-MacBook-Pro.local>
2025-09-18 19:34:45 +0530

    remove unused stuff

--- config.old/fish/functions/assume-role.fish
+++
@@ -1,8 +0,0 @@
-function assume-role
-  set --erase AWS_SECRET_ACCESS_KEY
-set --erase AWS_SESSION_TOKEN
-set --erase AWS_SECURITY_TOKEN
-set --erase ASSUMED_ROLE
-eval (command assume-role -duration=12h $argv)
-
-end
