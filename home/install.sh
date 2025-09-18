7e2c2f33f767ab2aa1eff5e196d046c22200fcd4
Farseen <itsfarseen@gmail.com>
2025-09-18 19:54:48 +0530

    migrate home dir stuff to the new style

--- home/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd $(dirname $0)
-
-ln -nsf $(realpath ./bashrc) $HOME/.bashrc
-ln -nsf $(realpath ./bash_profile) $HOME/.bash_profile
-ln -nsf $(realpath ./xprofile) $HOME/.xprofile
-mkdir -p $HOME/.cargo
-ln -nsf $(realpath ./cargo-config.toml) $HOME/.cargo/config.toml
