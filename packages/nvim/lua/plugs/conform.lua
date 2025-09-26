ca0b74bdfe3a9c4385eccf200f0d46194495fea6
Farseen <itsfarseen@gmail.com>
2025-09-26 22:49:35 +0530

    nvim: conform increase tiemout for formatting

--- packages/nvim/lua/plugs/conform.lua
+++ packages/nvim/lua/plugs/conform.lua
@@ -25,7 +25,7 @@ return {
 			lsp_format = "fallback",
 		},
 		-- Set up format-on-save
-		format_on_save = { timeout_ms = 500 },
+		format_on_save = { timeout_ms = 2000 },
 		-- Customize formatters
 		formatters = {
 			shfmt = {
