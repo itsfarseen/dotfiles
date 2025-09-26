db1d19581583360baa757247278aa14e40fc9fc4
Farseen <itsfarseen@gmail.com>
2025-09-26 23:00:47 +0530

    nvim: disable neoconf due to breakage
    https://github.com/folke/neoconf.nvim/issues/116

--- packages/nvim/lua/plugs-init.lua
+++ packages/nvim/lua/plugs-init.lua
@@ -1,3 +1,4 @@
-require("neoconf").setup()
+-- require("neoconf").setup()
+-- Disabled till this is fixed: https://github.com/folke/neoconf.nvim/issues/116
 require("fidget").setup(require("fidget-config"))
 require("config.lsp-config")
