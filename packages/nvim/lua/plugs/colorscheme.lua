73ba8a11f2cbea96c9861eb709bee3521e7ef2cd
Farseen <itsfarseen@gmail.com>
2026-05-02 13:13:08 +0530

    nvim: switch colorscheme to catppuccin

--- packages/nvim/lua/plugs/colorscheme.lua
+++ packages/nvim/lua/plugs/colorscheme.lua
@@ -1,14 +1,31 @@
 return {
+	-- {
+	-- 	"vague2k/vague.nvim",
+	-- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	-- 	priority = 1000, -- make sure to load this before all the other plugins
+	-- 	config = function()
+	-- 		-- NOTE: you do not need to call setup if you don't want to.
+	-- 		require("vague").setup({
+	-- 			-- optional configuration here
+	-- 		})
+	-- 		vim.cmd("colorscheme vague")
+	-- 	end,
+	-- },
 	{
-		"vague2k/vague.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other plugins
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 1000,
 		config = function()
-			-- NOTE: you do not need to call setup if you don't want to.
-			require("vague").setup({
-				-- optional configuration here
+			require("catppuccin").setup({
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				transparent_background = true,
+				float = {
+					transparent = true, -- enable transparent floating windows
+					solid = true, -- use solid styling for floating windows, see |winborder|
+				},
 			})
-			vim.cmd("colorscheme vague")
+			vim.cmd("colorscheme catppuccin")
 		end,
 	},
 }
