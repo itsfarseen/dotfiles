39904dc463328dcb92c88c324f26a1b352b8f01c
Farseen <itsfarseen@gmail.com>
2026-06-07 21:22:09 +0530

    fish: load rustup bin

--- packages/fish/conf.d/rustup.fish
+++ packages/fish/conf.d/rustup.fish
@@ -1,1 +1,6 @@
-test -f "$HOME/.cargo/env.fish"; and source "$HOME/.cargo/env.fish"
+test -f "$HOME/.cargo/env.fish"
+  and source "$HOME/.cargo/env.fish"
+
+test (uname) = Darwin
+  and test -d /opt/homebrew/opt/rustup/bin
+  and fish_add_path /opt/homebrew/opt/rustup/bin
